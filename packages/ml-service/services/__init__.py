247bc8f314aba94a81585159274597dca5d8254b
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2026-01-27 18:55:39 +0200

    feat: update init file for simplifying imports

--- packages/ml-service/services/__init__.py
+++ packages/ml-service/services/__init__.py
@@ -3,5 +3,6 @@ from .job_chunker_service import JobChunkerService
 from .skill_extractor_service import SkillExtractorService
 from .profile_pattern_regex import *
 from .skill_gap_service import analyze_skill_gap
+from .cv_generator_service import CVGeneratorService
 
-__all__ = ["EmbeddingService", "JobChunkerService", "SkillExtractorService", "analyze_skill_gap"]
+__all__ = ["EmbeddingService", "JobChunkerService", "SkillExtractorService", "analyze_skill_gap", "CVGeneratorService"]
