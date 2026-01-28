36b92f39d9966cd6f9620a80ee1710198165a7ab
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2026-01-28 17:42:34 +0200

    feat: create RTK endpoint for managing requests/responces to login, logout, register and get user

---
+++ packages/web/src/services/auth.ts
@@ -0,0 +1,53 @@
+import { AuthResponse, LoginRequest, RegisterRequest, User } from '../types'
+import { api } from './api.ts'
+
+export const authApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation<AuthResponse, LoginRequest>({
+      query: (credentials) => ({
+        url: 'auth/login',
+        method: 'POST',
+        body: credentials,
+      }),
+      onQueryStarted: async (_, { queryFulfilled }) => {
+        try {
+          const { data } = await queryFulfilled
+          localStorage.setItem('accessToken', data.accessToken)
+          localStorage.setItem('refreshToken', data.refreshToken)
+        } catch {}
+      },
+    }),
+
+    register: builder.mutation<{ message: string }, RegisterRequest>({
+      query: (credentials) => ({
+        url: '/auth/register',
+        method: 'POST',
+        body: credentials,
+      }),
+    }),
+
+    logout: builder.mutation<void, void>({
+      query: () => ({
+        url: 'auth/logout',
+        method: 'POST',
+        body: { refreshToken: localStorage.getItem('refreshToken') },
+      }),
+      onQueryStarted: async (_, { queryFulfilled }) => {
+        try {
+          await queryFulfilled
+        } finally {
+          localStorage.removeItem('accessToken')
+          localStorage.removeItem('refreshToken')
+        }
+      },
+    }),
+
+    getMe: builder.query<{ user: User }, void>({
+      query: () => '/auth/me',
+      providesTags: ['User'],
+    }),
+  }),
+})
+
+export const { useLoginMutation, useRegisterMutation, useLogoutMutation, useGetMeMutation } =
+  authApi
