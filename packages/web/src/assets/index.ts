6fb1f9623f36ca18826fc38333581ff3f4705d8d
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2026-01-30 21:39:20 +0200

    feat: add new background pictures for main page

--- packages/web/src/assets/index.ts
+++ packages/web/src/assets/index.ts
@@ -1,5 +1,8 @@
 import gridBg from './bg-lines.png';
 import logoSvg from './logo.svg';
 import logoTitleSvg from './logo_title.svg';
+import plate1 from './plate1.png';
+import plate2 from './plate2.png';
+import plate3 from './plate3.png';
 
-export { logoTitleSvg, gridBg, logoSvg };
+export { logoTitleSvg, gridBg, logoSvg, plate2, plate3, plate1 };
