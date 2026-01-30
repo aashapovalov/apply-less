f2ca0ff0645d3271682c10df0ad373b68437007e
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2026-01-30 08:49:56 +0200

    feat: add index files for layouts and pages to simplify imports

---
+++ packages/web/src/layout/index.ts
@@ -0,0 +1,4 @@
+import { AuthLayout } from '@/layout/auth-layout.tsx';
+import { MainLayout } from '@/layout/main-layout.tsx';
+
+export { MainLayout, AuthLayout };
---
+++ packages/web/src/pages/index.ts
@@ -0,0 +1,5 @@
+import { Landing } from '@/pages/landing.tsx';
+import { Login } from '@/pages/login.tsx';
+import { Register } from '@/pages/register.tsx';
+
+export { Login, Landing, Register };
