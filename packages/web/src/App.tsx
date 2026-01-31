85b4cd4946b618956c84b37a550cad0423526999
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2026-01-31 18:42:24 +0200

    fix: fix typo in 'reset password' route

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -33,7 +33,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset password" element={<ResetPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
       </Route>
     </Routes>
