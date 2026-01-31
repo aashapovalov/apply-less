cd0d3664d676ba735ae8434787c9e854b5b0fef6
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2026-01-31 20:54:54 +0200

    feat: add new file with constants for jobs list component

---
+++ packages/web/src/constants/index.ts
@@ -0,0 +1,10 @@
+export const JOBS_PER_PAGE = 20;
+
+export const REGION_LABELS: Record<string, string> = {
+  central: 'Central',
+  north: 'North',
+  south: 'South',
+  jerusalem: 'Jerusalem',
+  remote: 'Remote',
+  other: 'Other',
+};
