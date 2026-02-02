34156b329960f24ebd41c5510e45b7487ab3eb17
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2026-02-02 12:49:02 +0200

    feat: integrate not-found page in App and wrap routes in error boundary component

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { ErrorBoundary } from '@/components/error';
 import { AuthLayout, MainLayout } from '@/layout';
 import { ForgotPassword, Login, Register, ResetPassword, VerifyEmail } from '@/pages/auth';
-import { Landing } from '@/pages/general';
+import { Landing, NotFound } from '@/pages/general';
 import { JobDetails, Jobs } from '@/pages/jobs';
 import { Profile } from '@/pages/profile';
 
@@ -10,33 +11,37 @@ import { ProtectedRoute } from './components/auth';
 
 function App() {
   return (
-    <Routes>
-      {/* Public routes with main layout*/}
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Landing />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/jobs/:id" element={<JobDetails />} />
+    <ErrorBoundary>
+      <Routes>
+        {/* Public routes with main layout*/}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Landing />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/jobs/:id" element={<JobDetails />} />
 
-        {/* Protected routes */}
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-      </Route>
+          {/* Protected routes */}
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          {/* 404 - catch all */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
 
-      {/* Auth routes with auth layout*/}
-      <Route element={<AuthLayout />}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/verify-email" element={<VerifyEmail />} />
-      </Route>
-    </Routes>
+        {/* Auth routes with auth layout*/}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/verify-email" element={<VerifyEmail />} />
+        </Route>
+      </Routes>
+    </ErrorBoundary>
   );
 }
 
